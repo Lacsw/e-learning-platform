18e136c5b525a7bbc47edbbcdecc696cfd6395b5
Lacsw <frolloff1@yandex.ru>
2021-07-07 17:51:18 +0300

    add ContentCreateUpdateView to add different types of content in course modules

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, \
                                       DeleteView
@@ -6,8 +6,10 @@ from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin, \
                                        PermissionRequiredMixin
 from django.views.generic.base import TemplateResponseMixin, View
+from django.forms.models import modelform_factory
+from django.apps import apps
 
-from .models import Course
+from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
 
@@ -79,3 +81,62 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             return redirect('manage_course_list')
         return self.render_to_response({'course': self.course,
                                         'formset': formset})
+
+"""Добавление/редактирование нового контента(текст, видео, картинка, файл) в модуль"""
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    # Проверка, является ли модель одной из 4х типов
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(app_label='courses',
+                                  model_name=model_name)
+        return None
+
+    # Создание динамической формы с помощью modelform_factory
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner',
+                                                'order',
+                                                'created',
+                                                'updated'])
+        return Form(*args, **kwargs)
+
+    # Получение  параметров с URL и сохранение соответвующий в качестве атрибута класса
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(Module,
+                                        id=module_id,
+                                        course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model,
+                                        id=id,
+                                        owner=request.user)
+        return super().dispatch(request, module_id, model_name, id)
+
+    # При GET-запросе, создает форму модели
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form,
+                                        'object': self.obj})
+
+    # При POST-запросе, строит модель и передает ей полученные данные и файлы.
+    # Проверяет валидацию и устанавливает owner=user, сохраняет объект.
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,
+                            instance=self.obj,
+                            data=request.POST,
+                            files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                # новый контент
+                Content.objects.create(module=self.module,
+                                       item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form': form,
+                                        'object': self.obj})
