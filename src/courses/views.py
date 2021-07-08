d8da528133ee3ac329662d968be94695ee57cec1
frolovrv <frolloff1@yandex.ru>
2021-07-08 18:44:23 +0300

    create Content/Module ordering view by using JQuery UI and AJAX

--- src/courses/views.py
+++ src/courses/views.py
@@ -9,6 +9,8 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
 
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
@@ -164,3 +166,24 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    course__owner=request.user)
 
         return self.render_to_response({'module': module})
+
+"""Позволяет изменять порядок модулей"""
+class ModuleOrderView(CsrfExemptMixin,
+                         JsonRequestResponseMixin,
+                         View):
+       def post(self, request):
+           for id, order in self.request_json.items():
+               Module.objects.filter(id=id,
+                      course__owner=request.user).update(order=order)
+           return self.render_json_response({'saved': 'OK'})
+
+
+"""Позволяет измеянять порядок контента в модулях"""
+class ContentOrderView(CsrfExemptMixin,
+                         JsonRequestResponseMixin,
+                         View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                          module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
