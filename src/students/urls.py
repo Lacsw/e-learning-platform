d532a8603b6e0a44c959fbc1968d40707c25eb2c
frolovrv <frolloff1@yandex.ru>
2021-07-09 16:13:21 +0300

    create students registration view, template

---
+++ src/students/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+urlpatterns = [
+    path('register/',
+         views.StudentRegistrationView.as_view(),
+         name='student_registration'),
+]
