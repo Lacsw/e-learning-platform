802a4fb079018632c77b94480ceea4c9bd7ae6e6
frolovrv <frolloff1@yandex.ru>
2021-07-09 22:47:23 +0300

    add rendering different types of content templates

--- src/app/settings.py
+++ src/app/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3d-party app
+    'embed_video',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 from django.urls import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
